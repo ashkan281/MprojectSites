96ab5aff817d9bf8a0a02bd634de59b97122f89c
ashkan pourbabki <ashkan5fdp@gmail.com>
2025-09-30 14:32:28 +0330

    change setting by start project

--- Mysites/Mysites/urls.py
+++ Mysites/Mysites/urls.py
@@ -3,6 +3,6 @@ from django.urls import path , include
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/' , views.show_home),
+    path('' , views.show_home),
 ]
 
--- Mysites/Mysites/views.py
+++ Mysites/Mysites/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def show_home(request):
+    
     return render(request , 'index.html')
 

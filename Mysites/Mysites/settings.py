a29becd3f74d9e2c5ff7b67692fab622aa4e3674
ashkan pourbabki <ashkan5fdp@gmail.com>
2025-10-01 12:45:24 +0330

    finish project front

--- Mysites/Mysites/settings.py
+++ Mysites/Mysites/settings.py
@@ -114,8 +114,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'assets/'
-STATICFILES_DIRS =  [os.path.join(BASE_DIR ,  'assets')]
+STATIC_URL = 'assests/'
+STATICFILES_DIRS =  [os.path.join(BASE_DIR ,  'assests')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

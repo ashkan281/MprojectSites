0a3caf769c0c43e51b11edfa05e91a2fd67ec4f7
ashkan pourbabki <ashkan5fdp@gmail.com>
2025-10-14 11:07:11 +0330

    add page login finish

--- Mysites/Mysites/urls.py
+++ Mysites/Mysites/urls.py
@@ -6,6 +6,7 @@ from . import views , settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , views.show_home),
+    path('' , include('login_page.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
